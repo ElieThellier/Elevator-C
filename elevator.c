d06c2980b2ac9fe3ac3738f118f52c760204050c
ElieThellier <eliethellier@gmail.com>
2022-06-24 10:32:04 +0200

    definition des nouveaux types -> ça affiche l'ascenseur sans la fonction stepElevator

--- elevator.c
+++ elevator.c
@@ -1,16 +1,24 @@
 #include "elevator.h"
+#include <stdlib.h>
 
 void stepElevator(Building *b){
     
 }
 
 Elevator *create_elevator(int capacity, int currentFloor, PersonList *persons){
-    Elevator *e;
+    Elevator *e = malloc(sizeof(Elevator));
+    e -> capacity= capacity;
+    e -> currentFloor= currentFloor;
+    e -> targetFloor= currentFloor; //je sais pas encore quoi mettre
+    e -> persons = persons;
     return e;
 }
 
 Building *create_building(int nbFloor, Elevator *elevator, PersonList **waitingLists){
-    Building* b;
+    Building* b = malloc(sizeof(Building));
+    b -> nbFloor = nbFloor;
+    b -> elevator = elevator;
+    b -> waitingLists = waitingLists;
     return b;
 }
 
--- person.c
+++ person.c
@@ -1,11 +1,17 @@
 #include"person.h"
+#include<stdlib.h>
 
 Person* createPerson(int src, int dest){
-    Person* p;
+    Person* p=malloc(sizeof(Person));
+    p -> src = src;
+    p -> dest = dest;
     return p;
 }
 
 PersonList* insert(Person *p, PersonList *list){
-    PersonList* L;
+    // liste chainée avec liste vide char *p=NULL; au depart
+    PersonList* L=malloc(sizeof(PersonList));
+    L -> person=p;
+    L -> next = list;
     return L;
 }
